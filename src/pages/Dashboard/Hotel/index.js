bb58cc47b9422be8b4078b83e866fb88bc2d7433
Felipe Sakuma <fycorsak@gmail.com>
2022-06-09 13:00:14 -0300

    feat: add accomodation page title

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,38 +1,17 @@
-import { useContext } from 'react';
+import { Typography } from '@material-ui/core';
 import styled from 'styled-components';
-import TicketContext from '../../../contexts/TicketContext';
+import Content from './Content';
 
 export default function Hotel() {
-  const { ticket } = useContext(TicketContext);
-
-  if (!ticket.payment) {
-    return (
-      <CenterChildren>
-        <div>Você precisa ter confirmado pagamento antes
-          de fazer a escolha de hospedagem</div>
-      </CenterChildren>
-    );
-  }
-  if (!ticket.hotel) {
-    return (
-      <CenterChildren>
-        <div>Sua modalidade de ingresso não inclui hospedagem Prossiga para a escolha de atividades</div>
-      </CenterChildren>
-    );
-  }
-  return 'Hotel: Em breve!';
+  return (
+    <>
+      <StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>
+      <Content />
+    </>
+  );
 }
 
-const CenterChildren = styled.div`
-  width: 100%;
-  height: calc(100% - 65px);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  div {
-    width: 388px;
-    text-align: center;
-    color: #8e8e8e;
-  }
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+  text-align: left;
 `;
