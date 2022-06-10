edaab0d5b2ade35a9b4144e80bf930ddbc18e6a6
Felipe Sakuma <fycorsak@gmail.com>
2022-06-10 11:49:12 -0300

    chore: fix ticket context

--- src/contexts/TicketContext.js
+++ src/contexts/TicketContext.js
@@ -8,7 +8,7 @@ If not, create an empty ticket and populate context
 
 */
 
-import { createContext } from 'react';
+import { createContext, useEffect } from 'react';
 import useTicket from '../hooks/api/useTicket';
 import useLocalStorage from '../hooks/useLocalStorage';
 
@@ -16,11 +16,11 @@ const TicketContext = createContext();
 export default TicketContext;
 
 export function TicketProvider({ children }) {
-  const { ticketData, ticketLoading } = useTicket();
-
+  const { ticket: ticketData, ticketLoading } = useTicket();
   const [ticket, setTicket] = useLocalStorage('ticket', { booked: false });
 
-  if (ticketData) setTicket({ ...ticketData, booked: true, payment: true });
-
+  useEffect(() => {
+    if (ticketData) setTicket({ ...ticketData, booked: true, payment: true });
+  }, []);
   return <TicketContext.Provider value={{ ticket, setTicket, ticketLoading }}>{children}</TicketContext.Provider>;
 }
