86d5a52523d470313869ebaef808c1200cfa9dac
Vitor <vitormuller66@gmail.com>
2022-06-14 16:15:56 -0300

    feat: verify ticket and payment to change activities

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,46 @@
+import styled from 'styled-components';
+import { Typography } from '@material-ui/core';
+import { useContext } from 'react';
+import TicketContext from '../../../contexts/TicketContext';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const { ticket } = useContext(TicketContext);
+
+  if (!ticket.payment) {
+    return (
+      <CenterChildren>
+        <div>Você precisa ter confirmado pagamento antes
+          de fazer a escolha de atividades</div>
+      </CenterChildren>
+    );
+  }
+  if (ticket.type === 'online') {
+    return (
+      <CenterChildren>
+        <div>Sua modalidade de ingresso não necessita escolher
+          atividade. Você terá acesso a todas as atividades.</div>
+      </CenterChildren>
+    );
+  }
+  return (
+    <StyledTypography variant="h4">Escolha de Atividades</StyledTypography>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+  text-align: left;
+`;
+const CenterChildren = styled.div`
+  width: 100%;
+  height: calc(100% - 65px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    width: 388px;
+    text-align: center;
+    color: #8e8e8e;
+  }
+`;
