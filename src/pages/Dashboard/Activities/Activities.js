8c7277b6a4ccec7004d853cc76f4da954cd6e3bc
Felipe Sakuma <fycorsak@gmail.com>
2022-06-15 23:17:19 -0300

    feat: view activites

--- src/pages/Dashboard/Activities/Activities.js
+++ src/pages/Dashboard/Activities/Activities.js
@@ -1,88 +1,71 @@
-import dayjs, { Dayjs } from 'dayjs';
 import styled from 'styled-components';
 
 export default function Activities({ activities }) {
-  const mainActivities = activities.filter((activity) => activity.place === 'main');
-  const sideActivities = activities.filter((activity) => activity.place === 'side');
-  const workshopActivities = activities.filter((activity) => activity.place === 'workshop');
-  const allActivities = [mainActivities, sideActivities, workshopActivities];
-
-  console.log({ mainActivities });
-  console.log({ sideActivities });
-  console.log({ workshopActivities });
   return (
     <>
-      <Places>
-        <div>Auditório Principal</div>
-        <div>Auditório Lateral</div>
-        <div>Sala de Workshop</div>
-      </Places>
       <Schedule>
-        {allActivities.map((activities) => (
-          <ActivitySchedule>
-            {activities.map((activity) => {
-              const startsAt = `2019-01-25T${activity.startsAt}`;
-              const endsAt = `2019-01-25T${activity.endsAt}`;
-              const durationInMinutes = dayjs(endsAt).diff(startsAt, 'minutes');
-              return (
-                <Activity duration={durationInMinutes}>
-                  <div>
-                    <div id="activityName">{activity.name}</div>
-                    <div id="activityDuration">{`${activity.startsAt} - ${activity.endsAt}`}</div>
-                  </div>
-                </Activity>
-              );
-            })}
-          </ActivitySchedule>
+        <Place column="main">Auditório Principal</Place>
+        <Place column="side">Auditório Lateral</Place>
+        <Place column="workshop">Sala de Workshop</Place>
+
+        {activities.map((activity) => (
+          <Activity
+            column={activity.place}
+            row={`time-${activity.startsAt} / time-${activity.endsAt}`.replace(/:/g, '')}
+          >
+            <div>
+              <div id="activityName">{activity.name}</div>
+              <div id="activityDuration">{`${activity.startsAt} - ${activity.endsAt}`}</div>
+            </div>
+          </Activity>
         ))}
       </Schedule>
     </>
   );
 }
+const Schedule = styled.div`
+  display: grid;
+  margin-top: 5vmin;
+  gap: 1vmin;
 
-const Places = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  margin-top: 61px;
+  grid-template-rows:
+    [places] 1fr
+    [time-0900] 1fr
+    [time-0930] 1fr
+    [time-1000] 1fr
+    [time-1030] 1fr
+    [time-1100] 1fr
+    [time-1130] 1fr
+    [time-1200] 1fr;
+
+  grid-template-columns:
+    [main] 1fr
+    [side] 1fr
+    [workshop] 1fr;
+`;
+
+const Place = styled.div`
+  grid-row: places;
+  grid-column: ${({ column }) => column};
+
   font-family: 'Roboto';
   font-style: normal;
   font-weight: 400;
   font-size: 17px;
   line-height: 20px;
   text-align: center;
-
   color: #7b7b7b;
-  padding-bottom: 13px;
-`;
-
-const Schedule = styled.div`
-  display: flex;
-  width: 100%;
-`;
-
-const ActivitySchedule = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 33.333%;
-  gap: 1vmin;
-  padding: 1vmin;
-  border-top: 1px solid #d7d7d7;
-
-  &:first-of-type,
-  &:last-of-type {
-    border-right: 1px solid #d7d7d7;
-    border-left: 1px solid #d7d7d7;
-  }
 `;
 
 const Activity = styled.div`
+  grid-row: ${({ row }) => row};
+  grid-column: ${({ column }) => column};
+
   background-color: #f1f1f1;
-  display: flex;
   padding: 1vmin;
-  min-height: 79px;
-  height: ${({ duration }) => `${(duration * 79) / 60}px`};
   border-radius: 5px;
+  cursor: pointer;
+
   #activityName {
     font-family: 'Roboto';
     font-style: normal;
@@ -99,6 +82,7 @@ const Activity = styled.div`
     font-weight: 400;
     font-size: 12px;
     line-height: 14px;
+    margin-top: 6px;
 
     color: #343434;
   }
